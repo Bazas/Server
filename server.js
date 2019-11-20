15d73f03b7703d78c324110f6dbf5366390a4a7e
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2019-11-20 15:41:49 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const http = require('http')
+// Costante criada com o nome de http que recebe requisição do modulo http do nodeJS.
+
+const port = 3000;
+// Definido o número da porta localhost.
+
+const server = http.createServer((req, res) => {
+
+  res.end("")
+  // servidos inicia o que está na parte res.end.
+})
+server.listen(port, () => {
+  console.log(`Servidor iniciou em http://localhost:${port}/`)
+})
